7a9a9667321802fb193610bcbb9b9137ce6c3e99
Franciszek Wójcik <f.wojcikk@gmail.com>
2024-03-08 21:48:20 +0900

    Introduced stratified sampling of the test set

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import fetch_data
 import manipulate_data
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import StratifiedShuffleSplit
 
 
 fetch_data.fetch_housing_data()
@@ -11,15 +14,15 @@ head = housing.head()
 print(head)
 
 housing.hist(bins=50, figsize=(20,15))
-plt.show()
+# plt.show()
 
-train_set, test_set = manipulate_data.split_train_test(housing, 0.2)
+housing["income_cat"] = pd.cut(housing["median_income"],
+    bins=[0., 1.5, 3.0, 4.5, 6., np.inf],
+    labels=[1, 2, 3, 4, 5])
 
-print(len(train_set))
-print(len(test_set))
+split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+for train_index, test_index in split.split(housing, housing["income_cat"]):
+    strat_train_set = housing.loc[train_index]
+    strat_test_set = housing.loc[test_index]
 
-housing["id"] = housing["longitude"] * 1000 + housing["latitude"]
-train_set, test_set = manipulate_data.split_train_test_by_id(housing, 0.2, "id")
-
-head = housing.head()
 
