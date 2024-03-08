e67fc0c6cf1c5eab8b26742ed12d999bc0b1abbf
Franciszek Wójcik <f.wojcikk@gmail.com>
2024-03-08 17:00:30 +0900

    Added test set separation by unique longtitude and latitude based id

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 import fetch_data
-import housing as hs
+import manipulate_data
+import matplotlib.pyplot as plt
+
 
 fetch_data.fetch_housing_data()
-housing = hs.load_housing_data()
+housing = manipulate_data.load_housing_data()
 
 head = housing.head()
 
 print(head)
+
+housing.hist(bins=50, figsize=(20,15))
+plt.show()
+
+train_set, test_set = manipulate_data.split_train_test(housing, 0.2)
+
+print(len(train_set))
+print(len(test_set))
+
+housing["id"] = housing["longitude"] * 1000 + housing["latitude"]
+train_set, test_set = manipulate_data.split_train_test_by_id(housing, 0.2, "id")
+
+head = housing.head()
+
---
+++ manipulate_data.py
@@ -0,0 +1,25 @@
+import os
+import pandas as pd
+import numpy as np
+from zlib import crc32
+
+HOUSING_PATH = os.path.join("datasets", "housing")
+
+def load_housing_data(housing_path=HOUSING_PATH):
+ csv_path = os.path.join(housing_path, "housing.csv")
+ return pd.read_csv(csv_path)
+
+def split_train_test(data, test_ratio):
+ shuffled_indices = np.random.permutation(len(data))
+ test_set_size = int(len(data) * test_ratio)
+ test_indices = shuffled_indices[:test_set_size]
+ train_indices = shuffled_indices[test_set_size:]
+ return data.iloc[train_indices], data.iloc[test_indices]
+
+def test_set_check(identifier, test_ratio):
+ return crc32(np.int64(identifier)) & 0xffffffff < test_ratio * 2**32
+
+def split_train_test_by_id(data, test_ratio, id_column):
+ ids = data[id_column]
+ in_test_set = ids.apply(lambda id_: test_set_check(id_, test_ratio))
+ return data.loc[~in_test_set], data.loc[in_test_set]
