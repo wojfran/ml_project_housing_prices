7cf152f146a1873675064b5c6431f2f1f62b89a0
Franciszek Wójcik <f.wojcikk@gmail.com>
2024-03-13 09:25:10 +0900

    Finished the hands on project, will try to create one on my own soon

--- main.py
+++ main.py
@@ -8,6 +8,17 @@ from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import OrdinalEncoder
 from sklearn.preprocessing import OneHotEncoder
 import custom_transformer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.compose import ColumnTransformer
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import cross_val_score
+from sklearn.ensemble import RandomForestRegressor
+import joblib
+from sklearn.model_selection import GridSearchCV
+from scipy import stats
 
 fetch_data.fetch_housing_data()
 housing = manipulate_data.load_housing_data()
@@ -47,8 +58,8 @@ plt.show()
 housing["rooms_per_household"] = housing["total_rooms"]/housing["households"]
 housing["bedrooms_per_room"] = housing["total_bedrooms"]/housing["total_rooms"]
 housing["population_per_household"]=housing["population"]/housing["households"]
-corr_matrix = housing.corr()
-print(corr_matrix["median_house_value"].sort_values(ascending=False))
+# corr_matrix = housing.corr()
+# print(corr_matrix["median_house_value"].sort_values(ascending=False))
 
 housing = strat_train_set.drop("median_house_value", axis=1)
 housing_labels = strat_train_set["median_house_value"].copy()
@@ -143,3 +154,133 @@ housing_extra_attribs = attr_adder.transform(housing.values)
 # standardization.
 
 
+# Explanation on page 100 and 101 in the pdf but basically a pipeline condesnes
+# all of the transformers we have been using into one bigger transformer, which
+# is convenient
+
+num_pipeline = Pipeline([
+    ('imputer', SimpleImputer(strategy="median")),
+    ('attribs_adder', custom_transformer.CombinedAttributesAdder()),
+    ('std_scaler', StandardScaler()),
+ ])
+
+num_attribs = list(housing_num)
+cat_attribs = ["ocean_proximity"]
+
+full_pipeline = ColumnTransformer([
+    ("num", num_pipeline, num_attribs),
+    ("cat", OneHotEncoder(), cat_attribs),
+])
+
+housing_prepared = full_pipeline.fit_transform(housing)
+
+lin_reg = LinearRegression()
+lin_reg.fit(housing_prepared, housing_labels)
+
+# some_data = housing.iloc[:5]
+# some_labels = housing_labels.iloc[:5]
+# some_data_prepared = full_pipeline.transform(some_data)
+# print("Predictions:", lin_reg.predict(some_data_prepared))
+# print("Labels:", list(some_labels))
+
+housing_predictions = lin_reg.predict(housing_prepared)
+lin_mse = mean_squared_error(housing_labels, housing_predictions)
+lin_rmse = np.sqrt(lin_mse)
+print(lin_rmse)
+
+tree_reg = DecisionTreeRegressor()
+tree_reg.fit(housing_prepared, housing_labels)
+
+housing_predictions = tree_reg.predict(housing_prepared)
+tree_mse = mean_squared_error(housing_labels, housing_predictions)
+tree_rmse = np.sqrt(tree_mse)
+print(tree_rmse)
+
+scores = cross_val_score(tree_reg, housing_prepared, housing_labels,
+ scoring="neg_mean_squared_error", cv=10)
+tree_rmse_scores = np.sqrt(-scores)
+
+def display_scores(scores):
+    print("Scores:", scores)
+    print("Mean:", scores.mean())
+    print("Standard deviation:", scores.std())
+
+display_scores(tree_rmse_scores)
+               
+lin_scores = cross_val_score(lin_reg, housing_prepared, housing_labels, 
+                             scoring="neg_mean_squared_error", cv=10)
+
+lin_rmse_scores = np.sqrt(-lin_scores)
+display_scores(lin_rmse_scores)
+
+forest_reg = RandomForestRegressor()
+forest_reg.fit(housing_prepared, housing_labels)
+
+forest_scores = cross_val_score(forest_reg, housing_prepared, housing_labels,
+                         scoring="neg_mean_squared_error", cv=10)
+
+forest_rmse_scores = np.sqrt(-forest_scores)
+display_scores(forest_rmse_scores)
+
+joblib.dump(lin_reg, "lin_reg.pkl")
+joblib.dump(tree_reg, "tree_reg.pkl")
+joblib.dump(forest_reg, "forest_reg.pkl")
+
+# To load models:
+# my_model_loaded = joblib.load("my_model.pkl")
+
+param_grid = [
+    {'n_estimators': [3, 10, 30], 'max_features': [2, 4, 6, 8]},
+    {'bootstrap': [False], 'n_estimators': [3, 10], 'max_features': [2, 3, 4]},
+]
+
+forest_reg = RandomForestRegressor()
+grid_search = GridSearchCV(forest_reg, param_grid, cv=5, 
+                           scoring='neg_mean_squared_error', 
+                           return_train_score=True)
+grid_search.fit(housing_prepared, housing_labels)
+
+print(grid_search.best_params_)
+
+cvres = grid_search.cv_results_
+for mean_score, params in zip(cvres["mean_test_score"], cvres["params"]):
+    print(np.sqrt(-mean_score), params)
+
+
+# The grid search approach is fine when you are exploring relatively few combinations,
+# like in the previous example, but when the hyperparameter search space is large, it is
+# often preferable to use RandomizedSearchCV instead. This class can be used in much
+# the same way as the GridSearchCV class, but instead of trying out all possible combi‐
+# nations, it evaluates a given number of random combinations by selecting a random
+# value for each hyperparameter at every iteration. This approach has two main
+# benefits:
+# • If you let the randomized search run for, say, 1,000 iterations, this approach will
+# explore 1,000 different values for each hyperparameter (instead of just a few val‐
+# ues per hyperparameter with the grid search approach).
+# • Simply by setting the number of iterations, you have more control over the com‐
+# puting budget you want to allocate to hyperparameter search.
+
+# Testing the on the test set:
+    
+final_model = grid_search.best_estimator_
+X_test = strat_test_set.drop("median_house_value", axis=1)
+y_test = strat_test_set["median_house_value"].copy()
+X_test_prepared = full_pipeline.transform(X_test)
+
+final_predictions = final_model.predict(X_test_prepared)
+final_mse = mean_squared_error(y_test, final_predictions)
+final_rmse = np.sqrt(final_mse) # => evaluates to 47,730.2
+
+# In some cases, such a point estimate of the generalization error will not be quite
+# enough to convince you to launch: what if it is just 0.1% better than the model cur‐
+# rently in production? You might want to have an idea of how precise this estimate is.
+# For this, you can compute a 95% confidence interval for the generalization error using
+# scipy.stats.t.interval():
+
+confidence = 0.95
+squared_errors = (final_predictions - y_test) ** 2
+confidence_interval = np.sqrt(stats.t.interval(confidence, len(squared_errors) - 1, 
+                                               loc=squared_errors.mean(), 
+                                               scale=stats.sem(squared_errors)))
+
+print(confidence_interval)
